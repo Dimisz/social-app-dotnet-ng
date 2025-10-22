a830465c0b077bd0b205b220b07eebae762a7959
Vladimir Solovyov <leilyngale@icloud.com>
2025-10-22 10:53:41 +0800

    Store login state in localStorage

---
+++ client/src/core/services/account-service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../../types/user';
+import { tap } from 'rxjs';
+import { LoginCreds } from '../../types/login-creds';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
+
+  baseUrl = 'https://localhost:7007/api/';
+
+  login(creds: LoginCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/login', creds)
+      .pipe(
+        tap(user => {
+          if (user) {
+            localStorage.setItem('user', JSON.stringify(user));
+            this.currentUser.set(user)
+          }
+        })
+      )
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
